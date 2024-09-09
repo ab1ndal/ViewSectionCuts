a5eda1d2ebdd8a99fbaedd17471c959e9f4ab1e1
abindal-nyase <152557420+abindal-nyase@users.noreply.github.com>
2024-09-09 14:51:33 -0700

    Cleanup

--- utils/readFile.py
+++ utils/readFile.py
@@ -5,108 +5,97 @@ from dotenv import load_dotenv
 import os
 from sqlalchemy import create_engine, text
 from sqlalchemy.pool import NullPool
+from sqlalchemy.exc import SQLAlchemyError
 import psycopg2
 from psycopg2 import sql
 
-load_dotenv()
-
-base_db_url = os.getenv('DB_URL', 'postgresql+psycopg2://myuser:password@localhost:5432/MainFile')
+env_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+load_dotenv(env_path)
 
 db_params = {
-    'dbname': 'postgres',  # Default database for administrative tasks
-    'user': 'myuser',
-    'password': 'password',
-    'host': 'localhost',
-    'port': '5432'
+    'dbname': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASS'),
+    'host': os.getenv('DB_HOST'),
+    'port': os.getenv('DB_PORT')
 }
 
 def createConnection():
-    conn = psycopg2.connect(**db_params)
-    conn.autocommit = True
-    return conn
+    try:
+        conn = psycopg2.connect(**db_params)
+        conn.autocommit = False
+        return conn
+    except psycopg2.Error as e:
+        print(f'Error connecting to database: {e}')
+        raise
 
 def createTempDB(dbName):
     dropTempDB(dbName)
     conn = createConnection()
-    with conn.cursor() as cur:
-        cur.execute(sql.SQL('CREATE DATABASE {}').format(sql.Identifier(dbName)))
-    print(f'Database {dbName} created')
-    conn.close()
-    return f'postgresql+psycopg2://myuser:password@localhost:5432/{dbName}'
+    conn.autocommit = True
+    try:
+        with conn.cursor() as cur:
+            cur.execute(sql.SQL('CREATE DATABASE {}').format(sql.Identifier(dbName)))
+        print(f'Database {dbName} created')
+    except psycopg2.Error as e:
+        print(f'Error creating database {dbName}: {e}')
+        raise
+    return f'postgresql+psycopg2://{db_params["user"]}:{db_params["password"]}@{db_params["host"]}:{db_params["port"]}/{dbName}'
 
 def dropTempDB(dbName):
     conn = createConnection()
+    conn.autocommit = True
     with conn.cursor() as cur:
         cur.execute(sql.SQL('DROP DATABASE IF EXISTS {}').format(sql.Identifier(dbName)))
     print(f'Database {dbName} dropped')
-    conn.close()
-
 
 def getData(conn, **kwargs):
     tableName = kwargs.get('tableName')
     query = kwargs.get('query')
-
-    print(query)
-
     if not tableName and not query:
         raise ValueError('tableName or query must be provided')
-    
     if tableName:
         query = f'SELECT * FROM "{tableName}"'
-
     result = conn.execute(text(query))
     data = result.fetchall()
     columns = result.keys()
-
     df = pd.DataFrame(data, columns=columns)
-
-    print(df.head())
     return df
 
 def create_table_if_not_exists(connection, df, table_name):
-    # Create the table if it does not exist
     columns_with_types = ', '.join([f'"{col}" TEXT' for col in df.columns])
     create_table_query = f'''
     CREATE TABLE IF NOT EXISTS "{table_name}" (
         {columns_with_types}
     )
     '''
-    connection.execute(text(create_table_query))
+    try:
+        connection.execute(text(create_table_query))
+    except SQLAlchemyError as e:
+        print(f"Error occurred while creating table {table_name}: {e}")
+        raise
 
 def insert_data_bulk(connection, df, table_name):
-    # Ensure the table exists
     create_table_if_not_exists(connection, df, table_name)
-    
-    # Convert DataFrame to a list of dictionaries
-    data = df.to_dict(orient='records')
-    
-    # Prepare the SQL statement for bulk insert
-    columns = ", ".join([f'"{col}"' for col in df.columns])
-    placeholders = ", ".join([f":{col}" for col in df.columns])
-    
-    insert_query = f'''
-    INSERT INTO "{table_name}" ({columns})
-    VALUES ({placeholders})
-    '''
-    
-    # Execute the bulk insert
-    connection.execute(
-        text(insert_query),
-        data
-    )
+    connection.autoCommit = False
+    try:        
+        df.to_sql(
+            name=table_name, 
+            con=connection, 
+            if_exists='append', 
+            index=False,
+            method='multi')
+    except SQLAlchemyError as e:
+        print(f"Error occurred while inserting data into {table_name}: {e}")
+        raise
 
-def connectDB(filePath, DB_URL=base_db_url, dbName = 'MainFile'):
-    #baseEngine = create_engine(DB_URL, poolclass=NullPool)
-    #connection = baseEngine.connect()
+def connectDB(filePath, dbName = 'MainFile'):
+    tempDB_url = createTempDB(dbName)
+    engine = create_engine(tempDB_url, poolclass=NullPool)
+    connection = engine.connect()
     try:
-        tempDB_url = createTempDB(dbName)
-        engine = create_engine(tempDB_url, poolclass=NullPool)
-        connection = engine.connect()
-
-        start = time.time()
         xls = pd.ExcelFile(filePath)
         totalSheets = len(xls.sheet_names)
-
         for i, sheet in enumerate(xls.sheet_names,1):
             print(f'Processing {sheet}')
             progressVal = int((i-0.5/totalSheets)*100)
@@ -117,44 +106,6 @@ def connectDB(filePath, DB_URL=base_db_url, dbName = 'MainFile'):
             progressVal = int((i/totalSheets)*100)
             if progressVal != 100:
                 yield {'progress': progressVal, 'message': f'Processing {i} of {totalSheets} Sheets: {sheet}...'}
-        connection.commit()
-        end = time.time()
-        print(f'Execution time: {end - start}')
         yield connection
     except Exception as e:
-        print(e)
-        #dropTempDB(connection, dbName)
-        raise e
-
-
-def OLDconnectDB(filePath, connection=None):
-    #measure execution time
-    start = time.time()
-    if connection is None:
-        connection = sqlite3.connect(':memory:', check_same_thread=False)
-    xls = pd.ExcelFile(filePath)
-    totalSheets = len(xls.sheet_names)
-    for i, sheet in enumerate(xls.sheet_names,1):
-        print(f'Processing {sheet}')
-        progressVal = int((i-0.5/totalSheets)*100)
-        if progressVal < 100:
-            yield {'progress': progressVal, 'message': f'Processing {i} of {totalSheets} Sheets: {sheet}...'}
-        df = pd.read_excel(filePath, sheet_name=sheet, header=1).iloc[1:]
-        connection.execute('PRAGMA journal_mode=WAL;')
-        df.to_sql(sheet, connection, index=False, if_exists='replace')
-        progressVal = int((i/totalSheets)*100)
-        if progressVal != 100:
-            yield {'progress': progressVal, 'message': f'Processing {i} of {totalSheets} Sheets: {sheet}...'}
-    connection.commit()
-    end = time.time()
-    print(f'Execution time: {end - start}')
-    yield connection
-
-
-## Usage
-# filePath = r'C:\\Users\\abindal\\OneDrive - Nabih Youssef & Associates\\Documents\\00_Projects\\06_The Vault\\17JuneModels\\Part 302\\Displacement Study.xlsx'
-# resultFile = connectDB(filePath)
-# getData(resultFile, tableName='Groups 2 - Assignments')
-
-# query = 'SELECT * FROM "Groups 2 - Assignments"'
-# getData(resultFile, query=query)
+        raise
